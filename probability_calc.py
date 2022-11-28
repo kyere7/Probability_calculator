7f4869b1e57d2da177f68cabf2632ed1c6b6b41d
kyere7 <gyabaakyere@gmail.com>
2022-11-28 23:01:57 +0000

    modifications

--- probability_calc.py
+++ probability_calc.py
@@ -1,46 +1,48 @@
 import copy
 import random
-# Consider using the modules imported above.
+
 
 class Hat:
-    def __init__(self,**kwargs):
-        self.contents = list()
-        for key, val in kwargs.items():
-            for index in range(val):
-                self.contents.append(key)
-    
-    def draw(self, number):
-        try:
-            colors = random.sample(self.contents, number)
-        except:
-            colors = copy.deepcopy(self.contents)
+  def __init__(self, **kwargs):
+    self.contents = list()
+    for key, val in kwargs.items():
+      for index in range(val):
+        self.contents.append(key)
 
-        for color in colors:
-            self.contents.remove(color)
-        
-        return colors
+  def draw(self, number):
+    try:
+      colors = random.sample(self.contents, number)
+    except:
+      colors = copy.deepcopy(self.contents)
 
+    for color in colors:
+      self.contents.remove(color)
+
+    return colors
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    count = 0
-    for num in range(num_experiments):
-        hat_copy = copy.deepcopy(hat)
-        draw_colors = hat_copy.draw(num_balls_drawn)
-        xpected_balls_list = list()
-        for key, val in expected_balls.items():
-            xpected_balls_list.append(val)
-        
-        if ContainBalls(xpected_balls_list, draw_colors):
-            count += 1
-    
-    return count / num_experiments
+  count = 0
+  for i in range(num_experiments):
+    hat_copy = copy.deepcopy(hat)
+    balls = hat_copy.draw(num_balls_drawn)
 
-def ContainBalls(expected_balls, actual_balls):
-    for each_b in expected_balls:
-        if each_b in actual_balls:
-            actual_balls.remove(each_b)
-        else:
-            return False
-        return True
-    
+    exptd_ball_list = list()
+    for key, value in expected_balls.items():
+      for x in range(value):
+        exptd_ball_list.append(key)
+
+    if ContainsBall(exptd_ball_list, balls):
+      count += 1
+
+  probability = count / num_experiments
+  return probability
+
+
+def ContainsBall(expected_balls, actual_balls):
+  for b in expected_balls:
+    if b in actual_balls:
+      actual_balls.remove(b)
+    else:
+      return False
+  return True
